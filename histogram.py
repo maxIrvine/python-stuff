329c1f2214940bb06873bf0dca49945d323399cb
Max Irvine <mirvine46@gmail.com>
2017-06-26 13:21:30 -0400

    adds sorting method

--- histogram.py
+++ histogram.py
@@ -1,16 +1,29 @@
+import operator 
+
 def main():
+    dictionarySmall = {}
+    dictionaryBig = {}
+
+    def top(tally):
+        sorted_tally = sorted(tally.items(), key=operator.itemgetter(1))
+        print sorted_tally
+
     def letterSummary(word):
+        global dictionarySmall
         dictionary = {}
         for x in word:
             if (x not in dictionary.keys()):
                 dictionary[x] = 1
             elif (x in dictionary.keys()):
                 dictionary[x] += 1
+        dictionarySmall = dictionary
         print dictionary    
 
     letterSummary(raw_input("Desired word to count? "))
+    top(dictionarySmall)
     
     def wordSummary(sentence):
+        global dictionaryBig
         sentence = sentence.lower()
         sentenceList = sentence.split(" ")
         print sentenceList
@@ -20,7 +33,9 @@ def main():
                 dictionary[x] = 1
             elif (x in dictionary.keys()):
                 dictionary[x] += 1
-        print dictionary
-    
+        dictionaryBig = dictionary
+        print "Top = %s" % dictionary
+
     wordSummary(raw_input("Desired sentence to count? "))
+    top(dictionaryBig)
 main()
