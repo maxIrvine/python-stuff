13733882898e296dfa73e223f1d7a23c8807434a
Max Irvine <mirvine46@gmail.com>
2017-06-26 13:11:35 -0400

    adds wordSummary histogram

--- histogram.py
+++ histogram.py
@@ -8,6 +8,19 @@ def main():
                 dictionary[x] += 1
         print dictionary    
 
-    letterSummary(raw_input("Desired word? "))
+    letterSummary(raw_input("Desired word to count? "))
     
+    def wordSummary(sentence):
+        sentence = sentence.lower()
+        sentenceList = sentence.split(" ")
+        print sentenceList
+        dictionary = {}
+        for x in sentenceList:
+            if (x not in dictionary.keys()):
+                dictionary[x] = 1
+            elif (x in dictionary.keys()):
+                dictionary[x] += 1
+        print dictionary
+    
+    wordSummary(raw_input("Desired sentence to count? "))
 main()
